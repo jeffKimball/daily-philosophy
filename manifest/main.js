c4e5b695fd93bb3113147d3660576df32874b752
Jeff Kimball <jeffkimballis@gmail.com>
2021-11-13 23:11:15 -0600

    fix bug with CORS error

--- manifest/main.js
+++ manifest/main.js
@@ -3,7 +3,7 @@ function getQuotes() {
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
+            
             let quotes = data.quotes
             let randomIndex = Math.floor(Math.random() * quotes.length) 
             let quote = quotes[randomIndex]
@@ -14,3 +14,18 @@ function getQuotes() {
 }
 
 getQuotes()
+
+
+
+function getNews(){
+    fetch('https://dailyphilosophyapi.herokuapp.com/news')
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+        })
+
+        
+        
+}
+
+getNews()
