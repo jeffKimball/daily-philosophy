602994b590dc0f225e9e88294e805849ce928b54
Jeff Kimball <jeffkimballis@gmail.com>
2021-11-14 16:10:27 -0600

    fetch data from API and populate card elements

--- manifest/main.js
+++ manifest/main.js
@@ -1,3 +1,14 @@
+const showArticles = document.getElementById('show-articles')
+
+// Click event to display philosophy articles
+showArticles.addEventListener('click', () =>{
+    showArticles.style.display = "none"
+
+    document.getElementById('news').style.display = "flex"
+})
+
+
+
 // Fetch the quotes from quotes.json file and select one at random
 function getQuotes() {
     let url = '/quotes.json'
@@ -16,13 +27,20 @@ function getQuotes() {
 
 getQuotes()
 
-
+const title = document.querySelectorAll('#title')
+const link = document.querySelectorAll('#link')
 // Fetch philosophy articles from the webscraper api and populate cards
 function getNews(){
-    fetch('https://dailyphilosophyapi.herokuapp.com/news?limit=2')
+    fetch('https://dailyphilosophyapi.herokuapp.com/news/')
         .then((response) => response.json())
         .then((data) => {
-            console.log(data)
+            const newsArr = (data.slice(1,6))
+            console.log(newsArr)
+            for(let i = 0; i < newsArr.length;i++){
+                title[i].innerText = newsArr[i].title
+                link[i].href = newsArr[i].url
+            }
+            
         })
 
         
@@ -31,10 +49,3 @@ function getNews(){
 
 getNews()
 
-const showArticles = document.getElementById('show-articles')
-
-showArticles.addEventListener('click', () =>{
-    showArticles.style.display = "none"
-
-    document.getElementById('news').style.display = "flex"
-})
